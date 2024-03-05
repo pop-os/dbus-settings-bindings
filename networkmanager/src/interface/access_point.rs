5090164c645d575dfcf2bea8b2060e2ebfbb5df4
Ashley Wulber <ashley@system76.com>
2024-03-05 16:14:16 -0500

    chore(network-manager): more zbus updates

--- networkmanager/src/interface/access_point.rs
+++ networkmanager/src/interface/access_point.rs
@@ -20,50 +20,50 @@
 //!
 //! …consequently `zbus-xmlgen` did not generate code for the above interfaces.
 
-use zbus::dbus_proxy;
+use zbus::proxy;
 
-#[dbus_proxy(
+#[proxy(
 	interface = "org.freedesktop.NetworkManager.AccessPoint",
 	default_service = "org.freedesktop.NetworkManager"
 )]
 trait AccessPoint {
 	/// Flags property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn flags(&self) -> zbus::Result<u32>;
 
 	/// Frequency property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn frequency(&self) -> zbus::Result<u32>;
 
 	/// HwAddress property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn hw_address(&self) -> zbus::Result<String>;
 
 	/// LastSeen property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn last_seen(&self) -> zbus::Result<i32>;
 
 	/// MaxBitrate property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn max_bitrate(&self) -> zbus::Result<u32>;
 
 	/// Mode property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn mode(&self) -> zbus::Result<u32>;
 
 	/// RsnFlags property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn rsn_flags(&self) -> zbus::Result<u32>;
 
 	/// Ssid property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn ssid(&self) -> zbus::Result<Vec<u8>>;
 
 	/// Strength property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn strength(&self) -> zbus::Result<u8>;
 
 	/// WpaFlags property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn wpa_flags(&self) -> zbus::Result<u32>;
 }
--- networkmanager/src/interface/device/wireless.rs
+++ networkmanager/src/interface/device/wireless.rs
@@ -20,9 +20,9 @@
 //!
 //! …consequently `zbus-xmlgen` did not generate code for the above interfaces.
 
-use zbus::dbus_proxy;
+use zbus::proxy;
 
-#[dbus_proxy(
+#[proxy(
 	interface = "org.freedesktop.NetworkManager.Device.Wireless",
 	default_service = "org.freedesktop.NetworkManager"
 )]
@@ -40,45 +40,45 @@ pub trait WirelessDevice {
 	) -> zbus::Result<()>;
 
 	/// AccessPointAdded signal
-	#[dbus_proxy(signal)]
+	#[zbus(signal)]
 	fn access_point_added(&self, access_point: zbus::zvariant::ObjectPath<'_>) -> zbus::Result<()>;
 
 	/// AccessPointRemoved signal
-	#[dbus_proxy(signal)]
+	#[zbus(signal)]
 	fn access_point_removed(
 		&self,
 		access_point: zbus::zvariant::ObjectPath<'_>,
 	) -> zbus::Result<()>;
 
 	/// AccessPoints property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn access_points(&self) -> zbus::Result<Vec<zbus::zvariant::OwnedObjectPath>>;
 
 	/// ActiveAccessPoint property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn active_access_point(&self) -> zbus::Result<zbus::zvariant::OwnedObjectPath>;
 
 	/// Bitrate property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn bitrate(&self) -> zbus::Result<u32>;
 
 	/// HwAddress property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn hw_address(&self) -> zbus::Result<String>;
 
 	/// LastScan property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn last_scan(&self) -> zbus::Result<i64>;
 
 	/// Mode property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn mode(&self) -> zbus::Result<u32>;
 
 	/// PermHwAddress property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn perm_hw_address(&self) -> zbus::Result<String>;
 
 	/// WirelessCapabilities property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn wireless_capabilities(&self) -> zbus::Result<u32>;
 }
