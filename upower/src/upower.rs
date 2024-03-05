6fdb37c19fb56fd9477994ec5516132c5d6c4166
Ashley Wulber <ashley@system76.com>
2024-03-05 16:05:01 -0500

    chore(upower): zbus update

--- upower/src/upower.rs
+++ upower/src/upower.rs
@@ -1,11 +1,11 @@
 // Copyright 2021 System76 <info@system76.com>
 // SPDX-License-Identifier: MPL-2.0
 
-use zbus::dbus_proxy;
+use zbus::proxy;
 
 use crate::device::{DeviceProxy, DeviceProxyBlocking};
 
-#[dbus_proxy(interface = "org.freedesktop.UPower", assume_defaults = true)]
+#[proxy(interface = "org.freedesktop.UPower", assume_defaults = true)]
 trait UPower {
 	/// EnumerateDevices method
 	fn enumerate_devices(&self) -> zbus::Result<Vec<zbus::zvariant::OwnedObjectPath>>;
@@ -14,30 +14,30 @@ trait UPower {
 	fn get_critical_action(&self) -> zbus::Result<String>;
 
 	/// GetDisplayDevice method
-	#[dbus_proxy(object = "Device")]
+	#[zbus(object = "Device")]
 	fn get_display_device(&self);
 
 	/// DeviceAdded signal
-	#[dbus_proxy(signal)]
+	#[zbus(signal)]
 	fn device_added(&self, device: zbus::zvariant::ObjectPath<'_>) -> zbus::Result<()>;
 
 	/// DeviceRemoved signal
-	#[dbus_proxy(signal)]
+	#[zbus(signal)]
 	fn device_removed(&self, device: zbus::zvariant::ObjectPath<'_>) -> zbus::Result<()>;
 
 	/// DaemonVersion property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn daemon_version(&self) -> zbus::Result<String>;
 
 	/// LidIsClosed property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn lid_is_closed(&self) -> zbus::Result<bool>;
 
 	/// LidIsPresent property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn lid_is_present(&self) -> zbus::Result<bool>;
 
 	/// OnBattery property
-	#[dbus_proxy(property)]
+	#[zbus(property)]
 	fn on_battery(&self) -> zbus::Result<bool>;
 }
