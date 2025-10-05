5dab768fdb728152a019f98d36b408ccfa7f1dab
Vukašin Vojinović <150025636+git-f0x@users.noreply.github.com>
2025-10-05 14:17:34 +0200

    chore: clippy

--- mpris2/src/track.rs
+++ mpris2/src/track.rs
@@ -37,7 +37,7 @@ impl<'a> AsRef<ObjectPath<'a>> for TrackId {
 
 impl PartialOrd for TrackId {
 	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-		self.0.as_str().partial_cmp(other.0.as_str())
+		Some(self.cmp(other))
 	}
 }
 
