pop-os/dbus-settings-bindings

commit c31a46961a0bf1fa4a7bd77bb74716691c278348

chore: Rust 2024 edition

mpris2/src/enumerator.rs
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MPL-2.0
 
 use futures_util::{
-	stream::{self, StreamExt},
 	Stream,
+	stream::{self, StreamExt},
 };
-use zbus::{fdo::DBusProxy, names::OwnedBusName, Connection};
+use zbus::{Connection, fdo::DBusProxy, names::OwnedBusName};
 
 #[derive(Clone, Debug)]
 pub enum Event {

.rustfmt.toml
@@ -1,8 +1,4 @@
-edition = "2021"
+edition = "2024"
 hard_tabs = true
-merge_derives = true
 newline_style = "Unix"
-remove_nested_parens = true
-reorder_imports = true
-reorder_modules = true
 use_field_init_shorthand = true

a11y/Cargo.toml
@@ -1,7 +1,7 @@
 [package]
 name = "cosmic-dbus-a11y"
 version = "0.1.0"
-edition = "2021"
+edition = "2024"
 
 [dependencies]
 zbus.workspace = true

accounts-zbus/Cargo.toml
@@ -1,7 +1,7 @@
 [package]
 name = "accounts-zbus"
 version = "0.1.0"
-edition = "2021"
+edition = "2024"
 license = "MPL-2.0"
 
 [dependencies]

bluez/Cargo.toml
@@ -3,7 +3,7 @@ name = "bluez-zbus"
 version = "0.1.0"
 description = "dbus bindings for org.bluez with zbus"
 repository = "https://github.com/pop-os/dbus-settings-bindings"
-edition = "2021"
+edition = "2024"
 license = "MPL-2.0"
 categories = ["os::linux-apis"]
 keywords = ["dbus", "bluez", "zbus", "bluetooth"]

bluez/src/health_manager1.rs
@@ -32,5 +32,5 @@ pub trait HealthManager1 {
 
 	/// DestroyApplication method
 	fn destroy_application(&self, application: &zbus::zvariant::ObjectPath<'_>)
-		-> zbus::Result<()>;
+	-> zbus::Result<()>;
 }

bluez/src/lib.rs
@@ -45,7 +45,9 @@ pub async fn get_adapters<'a>(
 			.filter_map(std::result::Result::err)
 			.collect();
 		if errors.len() > 1 {
-			eprintln!("Multiple errors occurs when fetching connected device: {errors:?}. Only the last one will be returned.");
+			eprintln!(
+				"Multiple errors occurs when fetching connected device: {errors:?}. Only the last one will be returned."
+			);
 		}
 		return Err(errors.pop().unwrap());
 	}
@@ -165,7 +167,9 @@ pub async fn get_devices<'a>(
 			.filter_map(std::result::Result::err)
 			.collect();
 		if errors.len() > 1 {
-			eprintln!("Multiple errors occurs when fetching connected device: {errors:?}. Only the last one will be returned.");
+			eprintln!(
+				"Multiple errors occurs when fetching connected device: {errors:?}. Only the last one will be returned."
+			);
 		}
 		return Err(errors.pop().unwrap());
 	}

cosmic-settings-daemon/Cargo.toml
@@ -1,7 +1,7 @@
 [package]
 name = "cosmic-settings-daemon"
 version = "0.1.0"
-edition = "2021"
+edition = "2024"
 
 # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
 

commit 752d9a79d0e6ce54e20a6f2a895d13fd8e74acf5

fix(mpris2): regression in 2024 edition migration
Rust 2024 edition changed the lifetime capture rules, which broke
`Enumerator::receive_changes()`. Adding the `use<>` bound makes sure
that the lifetime isn't captured, like it was in the 2021 edition.

fixes: c31a46961a0bf1fa4a7bd77bb74716691c278348

mpris2/src/enumerator.rs
@@ -33,7 +33,7 @@ impl Enumerator {
 	/// Returns a stream that is signalled when an mpris client is added or removed
 	pub async fn receive_changes(
 		&self,
-	) -> zbus::Result<impl Stream<Item = zbus::Result<Event>> + Unpin> {
+	) -> zbus::Result<impl Stream<Item = zbus::Result<Event>> + Unpin + use<>> {
 		let stream = self.proxy.receive_name_owner_changed().await?;
 		Ok(stream
 			.filter_map(|signal| {